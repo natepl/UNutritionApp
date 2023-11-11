efbd1714c978e7b44e419fffaa596a5b1bdf8362
natepl <68605773+natepl@users.noreply.github.com>
2023-11-10 23:50:30 -0500

    Initial Commit

---
+++ UNutrition/UNutritionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UNutritionApp.swift
+//  UNutrition
+//
+//  Created by Nathaniel Parry Luff on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct UNutritionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
